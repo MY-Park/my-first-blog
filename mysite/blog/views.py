e35b6187d733273f606011a2d1f21735af58e9fc
Minyoung Park <mypark109@naver.com>
2020-06-10 21:13:47 +0900

    add

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,6 @@ from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-#from django.contrib.auth import login
-#from django.contrib.auth import logout
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -49,39 +47,6 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
-"""
-def login(request):
-    if request.method == 'POST':
-        login_form = AuthenticationForm(request, request.POST)
-        if login_form.is_valid():
-            auth_login(request, login_form.get_user())
-        return redirect('posts:list')
-
-    else:
-        login_form = AuthenticationForm()
-
-    return render(request, 'accounts/login.html', {'login_form': login_form})
-
-def logout(request):
-    auth_logout(request)
-    return redirect('posts:list')
-
-
-def signup(request):
-    # HTTP Method가 POST 인 경우
-    if request.method == 'POST':
-        signup_form = UserCreationForm(request.POST)
-    # 모델폼의 유효성 검증이 valid할 경우, DB에 저장
-    if signup_form.is_valid():
-        signup_form.save()
-        return redirect('posts:list')
-
-    # HTTP Method가 GET 인 경우
-    else:
-        signup_form = UserCreationForm()
-
-    return render(request, 'accounts/signup.html', {'signup_form': signup_form})
-"""
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
