5fd96aedc3a896c0cf0ea0ca54328acdfe9574f2
Minyoung Park <mypark109@naver.com>
2020-06-11 19:16:50 +0900

    setting login page as main, block unregistered users to see post list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,12 +3,33 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views.generic.edit import CreateView
+#from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.models import User
 
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import UserForm
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            login(request, new_user)
+        return redirect('/')
+    else:
+        form = UserForm()
+        return render(request, 'registration/signup.html', {'form': form})
+
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -48,6 +69,7 @@ def post_remove(request, pk):
     post.delete()
     return redirect('post_list')
 
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
